134176caa6fdcfc3597d45ea73e72ad8a2e6c301
Ganeshbyreddi <byreddiganesh768@gmail.com>
2026-09-01 16:59:11 +0530

    variables & keywords

---
+++ Day-2/Assigning values to variables.py
@@ -0,0 +1,23 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=10
+>>> A=20
+>>> a
+10
+>>> A
+20
+>>> a=b=c=a
+>>> a,b,c=10,20,30
+>>> a,b,c
+(10, 20, 30)
+>>> a
+10
+>>> b
+20
+>>> c
+30
+>>> a,b=b,a
+>>> a
+20
+>>> b
+10
