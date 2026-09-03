20257d634edcde92195a497eccb037fd6826ab5b
Ganeshbyreddi <byreddiganesh768@gmail.com>
2026-09-03 14:34:20 +0530

    datatypes & typeconvertions

---
+++ Day-3/datatypes.py
@@ -0,0 +1,84 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+count=10
+count=7
+count
+7
+type(count)
+<class 'int'>
+price=99.99
+price
+99.99
+type(price)
+<class 'float'>
+c=3+8j
+c
+(3+8j)
+c=4+9j
+c
+(4+9j)
+type(c)
+<class 'complex'>
+s='codegnan'
+s
+'codegnan'
+type(s)
+<class 'str'>
+s="code"
+s
+'code'
+type(s)
+<class 'str'>
+1=[]
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+1 = []
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+1=list[]
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+l=[]
+>>> l=list[]
+SyntaxError: invalid syntax
+>>> l=list()
+>>> type(1)
+<class 'int'>
+>>> type(l)
+<class 'list'>
+>>> l=[1,2,2,4,4,5,"dnbnvm",56.897,[1,2,5],(1,2)]
+>>> l
+[1, 2, 2, 4, 4, 5, 'dnbnvm', 56.897, [1, 2, 5], (1, 2)]
+>>> type(l)
+<class 'list'>
+>>> s={1,2,3,4,'23.1','8gerhgeh',23.2,'cnfr'
+...    s
+...    
+SyntaxError: '{' was never closed
+>>> s={1,2,3,4,'23.1','8gerhgeh',23.2,'cnfr'}
+...    
+>>> s
+...    
+{1, 2, 'cnfr', 3, 4, '23.1', 23.2, '8gerhgeh'}
+>>> type(s)
+...    
+<class 'set'>
+>>> status=None
+...    
+>>> type(status)
+...    
+<class 'NoneType'>
+>>> s={1,2,3,4,5}
+...    
+>>> s.add(6)
+...    
+>>> s
+...    
+{1, 2, 3, 4, 5, 6}
+>>> s.remove(2)
+...    
+>>> s
+...    
+{1, 3, 4, 5, 6}
+>>> s=frozenset({1,2,3,4})
+...    
+>>> s
+...    
+frozenset({1, 2, 3, 4})
---
+++ Day-3/type convertions.py
@@ -0,0 +1,12 @@
+#type convertions
+
+1.int        ->float,complex,str,bool
+2.float      ->int,complex,str,bool
+3.complex    ->str,bool
+4.str        ->int,float,complex,str,bool,list,tuple,set,frozenset
+5.list       ->str,bool,tuple,set,frozenset,dict
+6.tuple      ->str,bool,list,set,frozenset,dict
+7.set        ->str,bool,list,tuple,frozenset,dict
+8.dict       ->str,bool,list,tuple,set,frozenset,dict
+9.bool       ->int,float,complex,str,bool
+10.frozenset ->str,bool,list,tuple,set,dict
